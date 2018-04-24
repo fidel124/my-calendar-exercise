1b7566b5875ca794c0f71912379f028ad3cb4b7e
fidelis <fn_ololube@yahoo.com>
2018-04-24 16:09:57 +0300

    updated files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const normalizPort = port => parseInt(port, 10);
-const PORT = normalizPort(process.env.PORT || 5000);
+const PORT = normalizPort(process.env.PORT ||5000)
 
 const app = express();
 const dev = app.get('env') !== 'production'
